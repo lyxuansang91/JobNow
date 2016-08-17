ba23132e2354a539cecb2e89390c6320fada8fb3
manhnd <manh.it.it.it@gmail.com>
2016-08-17 15:45:33 +0700

    login fb

--- app/src/main/java/com/androidteam/jobnow/acitvity/SplashScreen.java
+++ app/src/main/java/com/androidteam/jobnow/acitvity/SplashScreen.java
@@ -1,29 +1,107 @@
 package com.androidteam.jobnow.acitvity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.androidteam.jobnow.BuildConfig;
 import com.androidteam.jobnow.R;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import org.json.JSONObject;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class SplashScreen extends AppCompatActivity {
 
     private Button btnLogin;
     private Button btnSignUp;
+    private Button btnLoginFacebook;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    BuildConfig.APPLICATION_ID,
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
         setupView();
+        initData();
+    }
+
+    private void initData() {
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(final LoginResult loginResult) {
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(final JSONObject object,
+                                                    GraphResponse response) {
+                                if (object != null) {
+//                                    CLog.d(TAG, " register fb: " + object.optString("id") + " " + object.optString("email"));
+//                                    editor.putString(Configruation.KEY_IDFB, object.optString("id"));
+//                                    editor.putString(Configruation.KEY_NAME, object.optString("name"));
+//                                    editor.commit();
+//                                    CLog.d(TAG, "token fb: " + loginResult.getAccessToken().getToken());
+//                                    EventBus.getDefault().post(new LoginSuccessEvent(LoginSuccessEvent.TYPE_FACEBOOK));
+                                    Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                                    startActivity(intent);
+                                } else {
+
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(SplashScreen.this, "Login Cancel", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(SplashScreen.this, error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     private void setupView() {
         btnLogin = (Button) findViewById(R.id.btnLogin);
         btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        btnLoginFacebook = (Button) findViewById(R.id.btnLoginFacebook);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,17 +117,33 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        btnLoginFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginFacebook();
+            }
+        });
     }
 
     long key_pressed;
 
     @Override
     public void onBackPressed() {
-        if(System.currentTimeMillis()- key_pressed < 2000) {
+        if (System.currentTimeMillis() - key_pressed < 2000) {
             super.onBackPressed();
         } else {
             Toast.makeText(getApplicationContext(), "Back again to exit", Toast.LENGTH_SHORT).show();
         }
         key_pressed = System.currentTimeMillis();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void loginFacebook() {
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile", "user_friends", "email"));
     }
 }
